dd3052151cdb9399799e7a035e8d763452314930
Jan Miksovsky <jan@miksovsky.com>
2025-09-21 09:35:34 -0700

    Tweaks

--- src/blog_demo/templates.py
+++ src/blog_demo/templates.py
@@ -1,3 +1,7 @@
+"""
+A folder of Jinja2 templates mapped to functions that apply those templates.
+"""
+
 from pathlib import Path
 
 from jinja2 import DictLoader, Environment
@@ -6,24 +10,26 @@ from map_origami import Folder, map_extensions, map_items
 
 from .site_info import site_info
 
+# Create a loader for the folder. Could also use FileSystemLoader, but we
+# already want to treat the folder as a map.
 here = Path(__file__).parent
-template_folder = Folder(here / "templates")
-template_texts = map_items(
-    template_folder,
-    value=lambda content: content.decode(
-        "utf-8") if isinstance(content, bytes) else content
-)
-env = Environment(loader=DictLoader(template_texts))
+folder = Folder(here / "templates")
+texts = map_items(folder, value=lambda b: b.decode("utf-8"))
+env = Environment(loader=DictLoader(texts))
+
+# Give templates access to site information
 env.globals["site_info"] = site_info
 
 
 def template_fn(content, template_name):
-    """Return a function that renders the given template with a context"""
+    """Return a function that renders the given template"""
+    # Instead of using the supplied content, we use the loader so that it can
+    # handle includes and extends.
     template = env.get_template(template_name)
     return lambda value, key, map: template.render(value=value, key=key, map=map)
 
 
 # Create a template function for each template file
 templates = map_extensions(
-    template_texts, extensions=".j2->", value=template_fn
+    texts, extensions=".j2->", value=template_fn
 )
